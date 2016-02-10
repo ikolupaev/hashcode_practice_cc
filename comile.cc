862d228677a7ddf7ce90801fc5ecea91122515bf
Igor Kolupaev <ikolupaev@hotmail.com>
2016-02-10 08:33:04 +0300

    improved score

--- comile.cc
+++ comile.cc
@@ -9,13 +9,22 @@ int columns;
 vector<string> data;
 vector<string> commands;
 
-bool is_dot( char ch ) { return ch == '#'; }
+bool is_filled( int x, int y ) { return data[x][y] == '#'; }
+
+bool clear( int x, int y ) { 
+    data[x][y] = '.'; 
+    cerr << "clear x: " << x << " y: " << y << " " << data[x][y] << endl; 
+}
 
 void PAINT_SQUARE( int x, int y, int size ) {
     ostringstream oss;
     oss << "PAINT_SQUARE " << x << " " << y << " " << size;
-    
     commands.push_back( oss.str() );
+    
+    for ( int row = x-size; row < x+size; row++ )
+        for ( int column = y-size; column< y+size; column++ )
+            clear( row, column );
+    
 }
 
 void load() {
@@ -33,15 +42,38 @@ void write() {
     for ( auto c : commands ) cout << c << endl;
 }
 
+bool is_square_clean( int x, int y, int size ) {
+    
+    for ( int row = x-size; row <= x+size; row++ )
+        for ( int column = y-size; column <= y+size; column++ )
+            if( is_filled( row, column ) ) return false;
+            
+    return true;
+}
+
+bool is_square_fit( int x, int y, int size ) {
+    
+    for ( int row = x-size; row <= x+size; row++ )
+        for ( int column = y-size; column <= y+size; column++ )
+            if( !is_filled( row, column ) ) return false;
+            
+    return true;
+}
+
+void fill_with_squares( int sq_size ) {
+    
+    cerr << "fill with sq size: " << sq_size << endl;
+    
+    for ( int x = sq_size; x+sq_size < rows; x++ ) 
+        for ( int y = sq_size; y+sq_size < columns; y++ ) 
+            if ( is_square_fit(x,y,sq_size) )
+                PAINT_SQUARE(x,y,sq_size);
+}
+
 void parse() {
-    int row = 0;
-    for ( auto s : data ) {
-        int column = 0;
-        for ( auto ch: s ) {
-            if( is_dot(ch) ) PAINT_SQUARE( row, column, 0 );
-            column++; 
-        }
-        row++;
+    
+    for ( int sq_size = min(rows, columns); sq_size >= 0; sq_size-- ) {
+        fill_with_squares( sq_size );
     }
 }
 
